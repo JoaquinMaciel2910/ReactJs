eb4c5664c81567ab5462d680ef6d9212bcc687e1
joaquin <joaquinmaciel2910@gmail.com>
2026-01-30 16:37:25 +0100

    added use reducer in taskApp

--- 04-hooks-app/src/main.tsx
+++ 04-hooks-app/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { PokemonPage } from './03-examples/PokemonPage'
 import { FocusScreen } from './04-useRef/FocusScreen'
+import { TasksApp } from './05-useReducer/taskApp'
 /* import { HooksApp } from './HooksApp'
 import { TrafficLight } from './01-useState/TrafficLight'
 import { TrafficLightEffect } from './02-useEffect/TrafficLightWithEffect'
@@ -15,6 +16,7 @@ createRoot(document.getElementById('root')!).render(
     {/* <TrafficLightEffect /> */}
     {/*     <TrafficLightWithHook />*/}
     {/* <PokemonPage /> */}
-    <FocusScreen />
+    {/* <FocusScreen /> */}
+    <TasksApp />
   </StrictMode>,
 )
